eea4ffd01066395cd85be97a1f319c3a4055232f
NikitaTerentev <fanxd@bk.ru>
2023-02-10 18:27:07 +0400

    added current assert

--- venv/Part3_final_task/test_items.py
+++ venv/Part3_final_task/test_items.py
@@ -7,5 +7,6 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_add_to_basket_button(browser):
     browser.get(link)
-    assert browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
+    browser.maximize_window()
+    assert browser.find_elements(By.CLASS_NAME, "btn-add-to-basket")
     time.sleep(3)
